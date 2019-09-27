f01e03f2f7b9f4e9ff8b1790b77ba40a37265991
Ángel barrios <ae.barrios@hoitmail.com>
2019-09-27 17:00:34 -0400

    muchos pendientes

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 function el(id) {
   return document.getElementById(id);
 }
+const imgCanvas = el('canvas')
+const canvas = new fabric.Canvas('canvas'); 
+let ctx = imgCanvas.getContext("2d");
+
+imgCanvas.onmousemove = ()=>{
+  let cRect = imgCanvas.getBoundingClientRect();
+  let x = Math.round(e.clientX - cRect.left);
+  let y = Math.round(e.clientY - cRect.top)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  console.log(x, y)
+}
 const botton2 = el('fileUpload2')
-const canvas = new fabric.Canvas('canvas');
+const croppied = el('crop')
+let imgScr = null
 
 botton2.onchange = function (e) {
   console.log(e)
@@ -17,10 +29,11 @@ botton2.onchange = function (e) {
         angle: 0,
         let: 0,
         top: 0,
-      }).scale(0.9)
+      })
       
       canvas.centerObject(imgFabri)
       imgFabri.set({globalCompositeOperation: 'destination-over'}); //set gCO for yellow
+      imgScr = imgFabri
       canvas.add(imgFabri)
     }
   }
@@ -40,5 +53,26 @@ var red = new fabric.Rect({
 });
 canvas.add(red);
 
+croppied.onclick =  function () {
 
+  let sourceX = 150;
+  let sourceY = 0;
+  let sourceWidth = 150;
+  let sourceHeight = 150;
+  let destWidth = sourceWidth;
+  let destHeight = sourceHeight;
+  let destX = canvas.width / 2 - destWidth / 2;
+  let destY = canvas.height / 2 - destHeight / 2;
+  
+  console.log(ctx)
+  
+  let note = document.createElement("img")
+  note.setAttribute('src', imgScr.toDataURL('image/jpg'))
+  imagendel = note.getAttribute('src')
+  
+  //ctx.drawImage(imagendel,inicioX,InicioY,desdex,desdey,destX,destY,destWidth,destHeight)
+  ctx.drawImage(imagendel,sourceX,sourceY,sourceWidth,sourceHeight,destX,destY,destWidth,destHeight)
+  document.body.insertBefore(note, croppied)
+  
+};
 
