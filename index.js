c419116ad8829a0b2c08e038869699045f47dbd4
Ángel barrios <ae.barrios@hoitmail.com>
2019-09-30 17:02:59 -0400

    no se

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 function el(id) {
   return document.getElementById(id);
 }
-const imgCanvas = el('canvas')
 const canvas = new fabric.Canvas('canvas'); 
+const imgCanvas = el('canvas')
 let ctx = imgCanvas.getContext("2d");
 
-imgCanvas.onmousemove = ()=>{
-  let cRect = imgCanvas.getBoundingClientRect();
-  let x = Math.round(e.clientX - cRect.left);
-  let y = Math.round(e.clientY - cRect.top)
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  console.log(x, y)
-}
+/* imgCanvas.onclick = (e) => {
+  var cRect = canvas.getBoundingClientRect();
+  //
+  var canvasX = Math.round(e.clientX - cRect.left); //
+  var canvasY = Math.round(e.clientY - cRect.top);
+  //
+  ctx.clearRect(0, 0, canvas.width, canvas.height); //
+  console.log("X: "+canvasX+", Y: "+canvasY);
+}; */
+
 const botton2 = el('fileUpload2')
 const croppied = el('crop')
 let imgScr = null
 
 botton2.onchange = function (e) {
-  console.log(e)
   var reader = new FileReader();
   reader.onload = function (f) {
-    console.log(f)
     let imObj = new Image()
     imObj.src = f.target.result;
     imObj.onload = () => {
@@ -46,33 +47,53 @@ var red = new fabric.Rect({
   width: 600,
   height: 200,
   strokeDashArray: [5, 5],
-  stroke: 'black',
+  stroke: '#00ff00',
   strokeWidth: 1,
   fill: 'transparent',
   selectable: false,
+  hasControls : false,
+  lockMovementX : true,
+  lockMovementY : true
 });
 canvas.add(red);
 
 croppied.onclick =  function () {
+  if (canvas.getActiveObject()){
+    
+    object = canvas.getActiveObject()
+    
+    object.left = red.left
+    object.top = red.top
+    
+    var eLeft = red.get('left');
+    var eTop = red.get('top');
+    var left = eLeft - object.left;
+    var top = eTop - object.top;
+    
+    left *= 1;
+    top *= 1;
 
-  let sourceX = 150;
-  let sourceY = 0;
-  let sourceWidth = 150;
-  let sourceHeight = 150;
-  let destWidth = sourceWidth;
-  let destHeight = sourceHeight;
-  let destX = canvas.width / 2 - destWidth / 2;
-  let destY = canvas.height / 2 - destHeight / 2;
-  
-  console.log(ctx)
-  
-  let note = document.createElement("img")
-  note.setAttribute('src', imgScr.toDataURL('image/jpg'))
-  imagendel = note.getAttribute('src')
-  
-  //ctx.drawImage(imagendel,inicioX,InicioY,desdex,desdey,destX,destY,destWidth,destHeight)
-  ctx.drawImage(imagendel,sourceX,sourceY,sourceWidth,sourceHeight,destX,destY,destWidth,destHeight)
-  document.body.insertBefore(note, croppied)
-  
+    var eWidth = red.get('width');
+    var eHeight = red.get('height');
+    var eScaleX = red.get('scaleX');
+    var eScaleY = red.get('scaleY');
+    var width = eWidth * 1;
+    var height = eHeight * 1;
+    console.log(-(eWidth / 2) + left)
+    console.log(-(eHeight / 2) + top)
+    
+      object.clipTo = (ctx)=>{
+        ctx.rect(-(eWidth / 2) + left, -(eHeight / 2) + top, parseInt(width * eScaleX), parseInt(eScaleY * height));
+      }
+      
+      //canvas.remove(red)
+      canvas.renderAll()
+    let note = document.createElement("img")
+    note.setAttribute('src', imgScr.toDataURL('image/jpg'))
+    imagendel = note.getAttribute('src')
+    document.body.insertBefore(note, botton2)
+  }else{
+    alert('seleccione la imagen')
+  }
 };
 
